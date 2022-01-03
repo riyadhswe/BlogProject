e7dbeb6d09ab3238c395cf2e34d251a8b4589d7a
riyadhswe <riyadhswe@gmail.com>
2022-01-03 21:08:36 +0600

    crispy

--- AppLogin/views.py
+++ AppLogin/views.py
@@ -5,9 +5,12 @@ from django.contrib.auth.forms import UserCreationForm
 def singup(request):
     form = UserCreationForm(data = request.POST)
     registered = False
-    if form.is_valid():
-        form.save()
-        registered = True
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            registered = True
+
 
     dict = {'form':form, 'registered':registered }
     return render(request,'AppLoginTemplate/sign_up.html',context = dict)
